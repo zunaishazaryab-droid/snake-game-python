292d301afb38a03726578b8e3f1c8112e4e13f95
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-14 12:28:11 +0500

    snake movement code refactor

--- main.py
+++ main.py
@@ -32,9 +32,14 @@ is_game_on = True
 
 while is_game_on:
      screen.update()
-     time.sleep(1)
-     for seg in segments:
-        seg.forward(20)
+     time.sleep(0.1)
+     for seg_num in range(len(segments) - 1, 0, -1 ):
+         new_x = segments[seg_num - 1].xcor()
+         new_y = segments[seg_num - 1].ycor()
+         segments[seg_num].goto(new_x, new_y)
+    
+     segments[0].forward(20)
+     segments[0].left(90)
        
         
 
