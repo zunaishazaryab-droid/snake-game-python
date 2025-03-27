347cb1262b89f0789ebc0d444ffdb734a156d998
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-27 12:35:32 +0500

    refactor file handling in scoreboard; improve high score retrieval and persistence logic

--- file_system.py
+++ file_system.py
@@ -1,10 +1,19 @@
 #only for practice purpose
-
-
-# file = open("my_file.txt")
+# file = open("new_file.txt")
 # content = file.read()
 # print(content)
 # file.close()
+#opening file with absolute path
+# file = open("/Users/arslanyousaf/Documents/new_file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+# opening file with relative path 
+file = open("../../Downloads/new_file.txt")
+content = file.read()
+print(content)
+file.close()
 
 
 # with open("my_file.txt") as file:
@@ -23,6 +32,6 @@
 
 
 # Create new file of the file did not exist
-with open("my_file_1.txt", mode="w") as file:
-    content = file.write("New Text.")
+# with open("my_file_1.txt", mode="w") as file:
+#     content = file.write("New Text.")
     
--- scoreboard.py
+++ scoreboard.py
@@ -7,31 +7,31 @@ class ScoreBoard(Turtle):
     
     
     def __init__(self):
-        file = open("data.txt", mode="r")
-        content = file.read()
+        
         
         super().__init__()
         self.score = 0
-        self.high_score = int(content)
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color("white")
         self.goto(0, 280)
         self.hideturtle()
         self.update_Score()
-        file.close()
+        
     
     # def game_over(self):
     #     self.goto(0,0)
     #     self.write(arg="Game Over", align=ALIGNMENT, font=FONT)
     
     def reset(self):
-        file = open("data.txt", mode="w")
-        
-        
-        if(self.score > self.high_score):
+        if self.score > self.high_score:
             self.high_score = self.score
-            file.write(str(self.high_score))
-            file.close()
+            try:
+                with open("data.txt", mode="w") as data:
+                    data.write(f"{self.high_score}")
+            except Exception as e:
+                print(f"Error writing to data.txt: {e}")
         self.score = 0
         self.update_Score()
     
