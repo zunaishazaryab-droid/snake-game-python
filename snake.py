5014b96debf41b53cc7319380dca4fbdca228eba
zunaishazaryab-droid <zunaishazaryab@gmail.com>
2025-08-27 21:59:54 -0700

    change the snake file

--- snake.py
+++ snake.py
@@ -2,10 +2,13 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0,0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-LEFT = 180
-RIGHT = 0
+
+aksdjaskdjsad
+dasdasdas
+dasdasdasd
+asdas
+d
+as
 
 class Snake:
     
